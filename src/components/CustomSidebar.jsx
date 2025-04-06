7a4abaeb0e85c5d169ecb8ad6a166c7c24145815
ryuniqlo <ryu1507@yahoo.com>
2025-04-07 01:10:23 +0800

    Added Report Generation

--- src/components/CustomSidebar.jsx
+++ src/components/CustomSidebar.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
@@ -5,9 +7,31 @@ import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 export default function CustomSidebar({ accessKeyId }) {
   const router = useRouter();
 
+  const [secretAccessKey, setSecretAccessKey] = useState("");
+  const [region, setRegion] = useState("");
+
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+  useEffect(() => {
+    const fetchCredentials = async () => {
+      try {
+        const res = await fetch(`${BASE_URL}/get-aws-credentials/`);
+        if (res.ok) {
+          const data = await res.json();
+          setSecretAccessKey(data.secret_access_key);
+          setRegion(data.default_region);
+        }
+      } catch (err) {
+        toast.error("Failed to load AWS credentials");
+      }
+    };
+
+    fetchCredentials();
+  }, []);
+
   const handleLogout = async () => {
     try {
-      const res = await fetch("http://localhost:8099/store-aws-credentials/", {
+      const res = await fetch(`${BASE_URL}/store-aws-credentials/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -27,6 +51,26 @@ export default function CustomSidebar({ accessKeyId }) {
       }
     } catch (err) {
       toast.error("Logout error:", err);
+    }
+  };
+
+
+  const handleGenerateReport = async () => {
+    try {
+      const res = await fetch(
+        `${BASE_URL}/generate-report/${accessKeyId}/${secretAccessKey}/${region}`
+      );
+      if (!res.ok) throw new Error("Failed to generate report");
+
+      const blob = await res.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "aws_cis_report.pdf";
+      a.click();
+      toast.success("Report downloaded!");
+    } catch (err) {
+      toast.error(err.message || "Error generating report");
     }
   };
 
@@ -58,6 +102,7 @@ export default function CustomSidebar({ accessKeyId }) {
             // <MenuItem> Remedies </MenuItem>
           }
         </SubMenu>
+        <MenuItem onClick={handleGenerateReport}> Generate Report </MenuItem>
         <MenuItem onClick={handleLogout}> Logout </MenuItem>
       </Menu>
     </Sidebar>
